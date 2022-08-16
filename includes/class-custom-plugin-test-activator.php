d2d96481947748207a3e1cf480e1f3ea9e16ce1a
Darwin <darwinsese@gmail.com>
2022-08-17 00:01:31 +0800

    initial

---
+++ includes/class-custom-plugin-test-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.facebook.com/
+ * @since      1.0.0
+ *
+ * @package    Custom_Plugin_Test
+ * @subpackage Custom_Plugin_Test/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Plugin_Test
+ * @subpackage Custom_Plugin_Test/includes
+ * @author     Darwin Sese < darwinsese@gmail.com>
+ */
+class Custom_Plugin_Test_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-custom-plugin-test-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.facebook.com/
+ * @since      1.0.0
+ *
+ * @package    Custom_Plugin_Test
+ * @subpackage Custom_Plugin_Test/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Custom_Plugin_Test
+ * @subpackage Custom_Plugin_Test/includes
+ * @author     Darwin Sese < darwinsese@gmail.com>
+ */
+class Custom_Plugin_Test_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
